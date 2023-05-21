5c26bf0c29f8bf4396572417b0eddc333ff5f531
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-05-21 20:35:26 +0530

    Screens-Components navigation

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Navbar } from '../components';
+
+const HomeScreen = () => {
+	return (
+		<View>
+			<Navbar />
+			<Text>HomeScreen</Text>
+		</View>
+	);
+};
+
+export default HomeScreen;
