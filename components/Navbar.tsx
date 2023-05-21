49367e84f2c8afac03534421bee98f36ecca897c
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-05-21 20:49:00 +0530

    react-native-heroicons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
-import { View } from 'react-native';
 import React from 'react';
 import { Button } from 'native-base';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { UserCircleIcon } from 'react-native-heroicons/solid';
+
 const Navbar = () => {
 	return (
 		<SafeAreaView>
+			<UserCircleIcon size={22} color="black" />
 			<Button>Navbar</Button>
 		</SafeAreaView>
 	);
